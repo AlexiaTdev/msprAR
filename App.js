ce53433d5edbc4ae16658b574ff38288339d79cc
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-08-12 14:48:07 +0200

    dependencies

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+import Contants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+
+const Stack = createStackNavigator()
+
+function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+      </Stack.Navigator>
     </View>
   );
+}
+
+export default() => {
+  return(
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eff',
+    marginTop:Contants.statusBarHeight
   },
 });
--- __tests__/Intro-test.js
+++ __tests__/Intro-test.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
+//import renderer from 'react-test-renderer';
 import Intro from '../Intro';
 
 test('rend correctement', () => {
-  const tree = renderer.create(<Intro />).toJSON();
-  expect(tree).toMatchSnapshot();
+  //const tree = renderer.create(<Intro />).toJSON();
+  //expect(tree).toMatchSnapshot();
+  const testRenderer = TestRenderer.create(
+    <Link page="https://www.facebook.com/">Facebook</Link>
+  );
+  
+  console.log(testRenderer.toJSON());
 });
